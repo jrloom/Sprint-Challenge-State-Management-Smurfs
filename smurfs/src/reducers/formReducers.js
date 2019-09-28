3d2b71b5fa3264611145fc31349abd1dc6aeaa81
Jason Loomis <jason@jrloomis.com>
2019-09-28 12:46:13 -0400

    reducers written

--- smurfs/src/reducers/formReducers.js
+++ smurfs/src/reducers/formReducers.js
@@ -1,7 +1,31 @@
-const initialState = {};
+import { POST_SMURFS, POST_SMURFS_SUCCESS, POST_SMURFS_FAILURE } from "../actions";
+
+const initialState = {
+  data: [],
+  isPosting: false,
+  error: ""
+};
 
 const formReducer = (state = initialState, action) => {
   switch (action.type) {
+    case POST_SMURFS:
+      return {
+        ...state,
+        isPosting: true
+      };
+    case POST_SMURFS_SUCCESS:
+      return {
+        ...state,
+        data: action.payload,
+        isPosting: false,
+        error: ""
+      };
+    case POST_SMURFS_FAILURE:
+      return {
+        ...state,
+        isPosting: false,
+        error: action.payload
+      };
     default:
       return state;
   }
--- smurfs/src/reducers/listReducers.js
+++ smurfs/src/reducers/listReducers.js
@@ -1,7 +1,31 @@
-const initialState = {};
+import { GET_SMURFS, GET_SMURFS_SUCCESS, GET_SMURFS_FAILURE } from "../actions";
+
+const initialState = {
+  data: [],
+  isGetting: false,
+  error: ""
+};
 
 const listReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_SMURFS:
+      return {
+        ...state,
+        isGetting: true
+      };
+    case GET_SMURFS_SUCCESS:
+      return {
+        ...state,
+        data: action.payload,
+        isGetting: false,
+        error: ""
+      };
+    case GET_SMURFS_FAILURE:
+      return {
+        ...state,
+        isGetting: false,
+        error: action.payload
+      };
     default:
       return state;
   }
